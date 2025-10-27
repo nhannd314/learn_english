a1d19a0e5b68fcb7dcf74c2cf381179dc4aa5b21
Nguyen Duy Nhan <nhannd314@gmail.com>
2025-10-28 06:57:40 +0700

    Fix course change upload image name and crop size

--- app/Filament/Resources/Courses/Schemas/CourseForm.php
+++ app/Filament/Resources/Courses/Schemas/CourseForm.php
@@ -5,7 +5,9 @@ namespace App\Filament\Resources\Courses\Schemas;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Components\Utilities\Get;
 use Filament\Schemas\Schema;
+use Illuminate\Support\Str;
 
 class CourseForm
 {
@@ -23,10 +25,24 @@ class CourseForm
                 FileUpload::make('thumbnail')
                     ->label('Thumbnail')
                     ->image()
+                    ->imageCropAspectRatio('3:4')
                     ->disk('public')
                     ->directory('courses')
                     ->visibility('public')
-                    ->maxSize(1024),
+                    ->maxSize(1024)
+                    ->getUploadedFileNameForStorageUsing(
+                        function ($file, Get $get): string {
+                            // Lấy title và tạo slug
+                            $title = $get('title');
+                            $slug = Str::slug($title);
+
+                            // Lấy extension của file
+                            $extension = $file->getClientOriginalExtension();
+
+                            // Trả về tên file: slug + extension
+                            return $slug . now()->timestamp . '.' . $extension;
+                        }
+                    ),
                 TextInput::make('order')
                     ->numeric()->default(0),
             ]);
